a32cd4bfefb19824166ee10fac9e41d47059943a
beeesssaaa <13708919+beeesssaaa@users.noreply.github.com>
2021-04-10 15:23:46 +0900

    Added Login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const signIn = () => {
+
+    }
+
     return (
-        <View>
-            <Text>I am the login screen</Text>
-        </View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Signal-Logo.svg/200px-Signal-Logo.svg.png"
+                }}
+                style={{width: 200, height: 200}}
+            />
+            <View style={styles.inputContainer}>
+                <Input placeholder="Email" autoFocus type="email" value={email} onChangeText={(text) => setEmail(text)}/>
+                <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={(text) => setPassword(text)}/>
+            </View>
+
+            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+            <Button containerStyle={styles.button} type="outline" title="Register" />
+            <View style={{height: 100}} />
+
+        </KeyboardAvoidingView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+    inputContainer: {
+        width: 300,
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+     }
+})
 
 export default LoginScreen
